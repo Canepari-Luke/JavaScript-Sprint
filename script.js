17255099e991c64bb394d66e9d13a168a150f344
Canepari-Luke <can20006@byui.edu>
2025-02-21 23:41:04 -0700

    Starting a JavaScript sprint for practice and review

---
+++ script.js
@@ -0,0 +1,9 @@
+// this prints hello world to the console.
+console.log("Hello World!");
+
+// this will add 3 and 5 and print the result to the console
+const x = 3 // const stands for constant veriable, a function cannot change it
+let y = 5 // let is a variable that a function could potentially change
+console.log(x + y);
+
+document.getElementById("total").innerHTML = `total = ${x + y}`; // This is how we are selecting the id in the html to change it

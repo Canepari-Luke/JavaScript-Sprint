656557200398149a817cb085b9c7d314f16bfc91
Canepari-Luke <can20006@byui.edu>
2025-02-23 15:57:12 -0700

    created a JavaScript button. Trying to build a math function.

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ let y = 5 // let is a variable that a function could potentially change
 console.log(x + y);
 
 document.getElementById("total").innerHTML = `total = ${x + y}`; // This is how we are selecting the id in the html to change it
+// the `total = ${x + y}` is called a template literal and is used to insert a variable into a string
+
+const button=document.getElementById("button"); // this defines the button from the HTML for the JavaScript
+
+// This adds functionality to the button by changing the HTML
+button.addEventListener("click", function(){document.getElementById('demo').innerHTML = 'This is JavaScript!'});
+
+
+const a = document.querySelector('.a').innerHTML;
+
+const b = document.querySelector('.b').innerHTML;
+
+const enter=document.getElementById("enter");
+
+enter.addEventListener("onClick", function(){parseInt(a); parseInt(b); add});
+
+function add(a, b){
+   let t = a + b;
+   let h = document.createElement("p");
+   document.querySelector(".math").appendChild(h);
+   h.innerHTML = `${t}`;
+}
